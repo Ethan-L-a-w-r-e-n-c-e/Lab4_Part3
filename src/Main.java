f70a889dd2a65cb495778f670237c9561dc72deb
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-12 08:23:56 -0400

    w

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double yearlyCost = 0;
+        double winterCost;
+        double fallCost;
+        double summerCost;
+        double springCost;
+
+        System.out.println("What was your maintenance cost for the winter");
+        winterCost = scan.nextDouble();
+        System.out.println("What was your maintenance cost for the spring");
+        springCost = scan.nextDouble();
+        System.out.println("What was your maintenance cost for the summer");
+        summerCost = scan.nextDouble();
+        System.out.println("What was your maintenance cost for the fall");
+        fallCost = scan.nextDouble();
+
+        yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Your yearly maintenance cost for is $" + yearlyCost + ".");
     }
 }
