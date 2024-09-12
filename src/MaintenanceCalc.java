ee50ab5a3cf3e3500d71c7db1421b10a258c740c
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-12 08:25:43 -0400

    ws

--- src/MaintenanceCalc.java
+++ src/MaintenanceCalc.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MaintenanceCalc {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
